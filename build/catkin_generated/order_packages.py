73198a5e6cc86c97f45aa09f41f4bf6b7920df1b
Aiswarya Anand <anandaiswarya2109@gmail.com>
2021-02-12 23:08:11 +0530

    Second commit

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/aiswarya/mybot_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
+underlay_workspaces = '/home/aiswarya/mybot_ws/devel;/opt/ros/melodic'.split(';') if '/home/aiswarya/mybot_ws/devel;/opt/ros/melodic' != '' else []
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,6 +131,9 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/aiswarya/mybot_ws/build/gtest/cmake_install.cmake")
+  include("/home/aiswarya/mybot_ws/build/mybot_control/cmake_install.cmake")
+  include("/home/aiswarya/mybot_ws/build/mybot_description/cmake_install.cmake")
+  include("/home/aiswarya/mybot_ws/build/mybot_gazebo/cmake_install.cmake")
 
 endif()
 
